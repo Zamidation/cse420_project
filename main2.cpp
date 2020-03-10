f302b56e3fe9ffe16d4fe85666cabca1d9ba0c9a
005444968 <005444968@csusb.edu@jb359-14.cse.csusb.edu>
2020-03-10 15:44:38 -0700

    Light switches

--- main2.cpp
+++ main2.cpp
@@ -6,23 +6,28 @@ void display( void );
 void init( void );
 void keyboard ( unsigned char key, int mousex, int mousey );
 void visHandle( int visible );
+void ending();
 
 int main( int argc, char *argv[] )
 {
   glutInit( &argc, argv );
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH );
-  glutInitWindowSize( 1600, 800 );
-  glutInitWindowPosition( 300, 100 );
+  glutInitWindowSize( 800, 800 );
+
+  glutInitWindowPosition( 100, 100 );
   glutCreateWindow("Solar System");
   init();
   
+  // glViewport(200,200,300,300);
+  glViewport(0,0,10,10);
+
   glutDisplayFunc(display);
   glutKeyboardFunc ( keyboard );
-  glViewport(0,0,800,800);
+  
   
   glutVisibilityFunc( visHandle );
   glutMainLoop();
-
+  ending();
   return 0;
 }
 
